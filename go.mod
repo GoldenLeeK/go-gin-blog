e6a1131c1ed10b76353728d8b74603a001298076
lijh@95you.com <GoldenLeeK>
2022-12-20 11:52:37 +0800

    新增endless

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/astaxie/beego v1.12.3
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/fvbock/endless v0.0.0-20170109170031-447134032cb6
 	github.com/gin-gonic/gin v1.8.1
 	github.com/go-ini/ini v1.67.0
 	github.com/jinzhu/gorm v1.9.16
--- main.go
+++ main.go
@@ -2,22 +2,29 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"log"
+	"syscall"
 
 	"github.com/GoldenLeeK/go-gin-blog/pkg/setting"
 	"github.com/GoldenLeeK/go-gin-blog/routers"
+
+	"github.com/fvbock/endless"
 )
 
 func main() {
-	router := routers.InitRouter()
 
-	s := &http.Server{
-		Addr:           fmt.Sprintf(":%d", setting.HTTPort),
-		Handler:        router,
-		ReadTimeout:    setting.ReadTimeout,
-		WriteTimeout:   setting.WriteTimeout,
-		MaxHeaderBytes: 1 << 20,
+	endless.DefaultReadTimeOut = setting.ReadTimeout
+	endless.DefaultWriteTimeOut = setting.WriteTimeout
+	endless.DefaultMaxHeaderBytes = 1 << 20
+	endPoint := fmt.Sprintf(":%d", setting.HTTPort)
+
+	server := endless.NewServer(endPoint, routers.InitRouter())
+	server.BeforeBegin = func(add string) {
+		log.Printf("Actual pid is %d", syscall.Getpid())
 	}
 
-	s.ListenAndServe()
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Printf("Server err: %v", err)
+	}
 }
